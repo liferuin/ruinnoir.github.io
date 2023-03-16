e7014b102ff27c7d9f0cec76dad7a3580bc72fc0
LIFERUIN <thetylermyers@gmail.com>
2023-03-16 12:46:54 -0400

    Fix index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
+let folder = "images/";
+
 $(document).ready(function(){
-
-    var folder = "tree/main/images/";
-
     $.ajax({
         url : folder,
         success: function (data) {
             $(data).find("a").attr("href", function (i, val) {
                 if( val.match(/\.(jpe?g|png|gif)$/) ) { 
                     $("div.content").append( "<span><img src='" + val + "'></span>" );
+                    var randomize = $("span");
+                    for(var i = 0; i < randomize.length; i++){
+                    var target = Math.floor(Math.random() * randomize.length -1) + 1;
+                    var target2 = Math.floor(Math.random() * randomize.length -1) +1;
+                    randomize.eq(target).before(randomize.eq(target2));
+    }
                 } 
             });
         }
