b419c8baaab1b9e7b23c4b61389e32cb3f7e6384
LIFERUIN <thetylermyers@gmail.com>
2023-03-16 08:38:08 -0400

    index.js fix

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         success: function (data) {
             $(data).find("a").attr("href", function (i, val) {
                 if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                    $("div.content").append( "<span><img src='"+ folder + val +"'></span>" );
+                    $("div.content").append( "<span><img src='"+ val +"'></span>" );
                 } 
             });
         }
